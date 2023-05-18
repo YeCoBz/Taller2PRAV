be4c898d18d80a6181a9b42f5e76796d7fd10ca9
Gorkham <113120841+Gorkham@users.noreply.github.com>
2023-05-18 19:22:26 -0400

    VIVA EL PERU CARAJO
    VIVA EL PERU PE

--- src/Taller2/App.java
+++ src/Taller2/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
 		String nombre = "cristian";
-
+		System.out.println("VIVA EL PERU CARAJO");
 	}
 
 }
