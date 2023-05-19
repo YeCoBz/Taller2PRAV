03fd5deb95753d2d275b61063fdaf2ac98703203
Gorkham <113120841+Gorkham@users.noreply.github.com>
2023-05-18 21:00:21 -0400

    Probando cosas

--- src/Taller2/App.java
+++ src/Taller2/App.java
@@ -4,6 +4,9 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
+		int valor1=0;
+		double porcentaje=0.0;
+		String hola = ola;
 		String nombre = "cristian";
 		System.out.println("VIVA EL PERU CARAJO");
 		int cantidad = 10;
