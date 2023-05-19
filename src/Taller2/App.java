51c580a26cbd14176cf36288b3ce53de5c2cf4c3
Gorkham <cristianurqueta23@gmail.com>
2023-05-18 21:04:28 -0400

    Arreglado

--- src/Taller2/App.java
+++ src/Taller2/App.java
@@ -6,7 +6,7 @@ public class App {
 		System.out.println("hola mundo");
 		int valor1=0;
 		double porcentaje=0.0;
-		String hola = ola;
+		String hola = "ola";
 		String nombre = "cristian";
 		System.out.println("VIVA EL PERU CARAJO");
 		int cantidad = 10;
